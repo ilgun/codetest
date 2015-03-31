80fb3e8750d8a3017bda675abf31f909ba8228b3
ilgün ilgün <ilgun.ilgun@gmail.com>
2015-03-31 23:19:39 +0300

    Implementation details extracted from the model.

--- src/main/java/elevator/Elevator.java
+++ src/main/java/elevator/Elevator.java
@@ -1,15 +1,9 @@
 package elevator;
 
-import com.google.common.base.Optional;
-
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.SortedMap;
 
-import static com.google.common.base.Optional.absent;
 import static com.google.common.collect.Maps.newTreeMap;
-import static java.lang.Integer.reverse;
 
 public class Elevator {
     // Map consists of floor , direction
@@ -62,57 +56,15 @@ public class Elevator {
         this.destinationFloor = destinationFloor;
     }
 
-    //TODO put in to IMPL.
-    //TODO destination floor is not needed, put it into the orders directly.
-    public void step() {
-        if (elevatorOrders.isEmpty()) {
-            currentFloor = destinationFloor;
-            destinationFloor = currentFloor;
-        } else {
-            currentFloor = getNextOrder().getKey();
-            elevatorOrders.remove(currentFloor);
-            if (!elevatorOrders.isEmpty()) {
-                destinationFloor = getNextOrder().getKey();
-            } else {
-                destinationFloor = currentFloor;
-            }
-        }
-    }
-
-
     public Integer getCurrentDirection() {
         return currentDirection;
     }
 
-    private Entry<Integer, Integer> getNextOrder() {
-        Optional<Entry<Integer, Integer>> nextOptimumOrder = getNextOptimumOrder(elevatorOrders.entrySet().iterator(), currentDirection);
-
-        if (nextOptimumOrder.isPresent()) {
-            Entry<Integer, Integer> nextOrder = nextOptimumOrder.get();
-            return nextOrder;
-        } else {
-            Entry<Integer, Integer> nextReverseOrder = getNextReverseOrder(elevatorOrders.entrySet().iterator(), reverse(currentDirection));
-            return nextReverseOrder;
-        }
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
     }
 
-    //Trying to get the currentDirection for next order.
-    private Optional<Entry<Integer, Integer>> getNextOptimumOrder(Iterator<Entry<Integer, Integer>> orderIterator, Integer currentDirection) {
-        while (orderIterator.hasNext()) {
-            Entry<Integer, Integer> next = orderIterator.next();
-            if (isNegative(next.getValue()) == isNegative(currentDirection)) {
-                return Optional.of(next);
-            }
-        }
-        return absent();
-    }
-
-    //If there is not current direction order, then get the reverse one.
-    private Entry<Integer, Integer> getNextReverseOrder(Iterator<Entry<Integer, Integer>> orderIterator, Integer currentDirection) {
-        return orderIterator.next();
-    }
-
-    private boolean isNegative(Integer l) {
-        return (Math.abs(l - 1) > Math.abs(l));
+    public void setDestinationFloor(int destinationFloor) {
+        this.destinationFloor = destinationFloor;
     }
 }
--- src/main/java/elevator/ElevatorControlSystemImpl.java
+++ src/main/java/elevator/ElevatorControlSystemImpl.java
@@ -1,10 +1,15 @@
 package elevator;
 
+import com.google.common.base.Optional;
 import org.apache.log4j.Logger;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import static com.google.common.base.Optional.absent;
 import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Integer.reverse;
 import static org.apache.log4j.Logger.getLogger;
 
 public class ElevatorControlSystemImpl implements ElevatorControlSystem {
@@ -49,7 +54,51 @@ public class ElevatorControlSystemImpl implements ElevatorControlSystem {
     @Override
     public void step() {
         for (Elevator elevator : elevators) {
-            elevator.step();
+            Map<Integer, Integer> elevatorOrders = elevator.getElevatorOrders();
+            if (elevatorOrders.isEmpty()) {
+                elevator.setCurrentFloor(elevator.getDestinationFloor());
+                elevator.setDestinationFloor(elevator.getCurrentFloor());
+            } else {
+                elevator.setCurrentFloor(getNextOrder(elevatorOrders, elevator.getCurrentDirection()).getKey());
+                elevatorOrders.remove(getNextOrder(elevatorOrders, elevator.getCurrentDirection()).getKey());
+                if (!elevatorOrders.isEmpty()) {
+                    elevator.setDestinationFloor(getNextOrder(elevatorOrders, elevator.getCurrentDirection()).getKey());
+                } else {
+                    elevator.setDestinationFloor(elevator.getCurrentFloor());
+                }
+            }
         }
+    }
+
+    private Map.Entry<Integer, Integer> getNextOrder(Map<Integer, Integer> elevatorOrders, Integer currentDirection) {
+        Optional<Map.Entry<Integer, Integer>> nextOptimumOrder = getNextOptimumOrder(elevatorOrders.entrySet().iterator(), currentDirection);
+
+        if (nextOptimumOrder.isPresent()) {
+            Map.Entry<Integer, Integer> nextOrder = nextOptimumOrder.get();
+            return nextOrder;
+        } else {
+            Map.Entry<Integer, Integer> nextReverseOrder = getNextReverseOrder(elevatorOrders.entrySet().iterator(), reverse(currentDirection));
+            return nextReverseOrder;
+        }
+    }
+
+    //Trying to get the currentDirection for next order.
+    private Optional<Map.Entry<Integer, Integer>> getNextOptimumOrder(Iterator<Map.Entry<Integer, Integer>> orderIterator, Integer currentDirection) {
+        while (orderIterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = orderIterator.next();
+            if (isNegative(next.getValue()) == isNegative(currentDirection)) {
+                return Optional.of(next);
+            }
+        }
+        return absent();
+    }
+
+    //If there is not current direction order, then get the reverse one.
+    private Map.Entry<Integer, Integer> getNextReverseOrder(Iterator<Map.Entry<Integer, Integer>> orderIterator, Integer currentDirection) {
+        return orderIterator.next();
+    }
+
+    private boolean isNegative(Integer l) {
+        return (Math.abs(l - 1) > Math.abs(l));
     }
 }
