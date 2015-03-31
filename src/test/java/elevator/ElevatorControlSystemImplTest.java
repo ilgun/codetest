b06266e17769e6d6016c7b5e87183b92e12ea3af
ilgün ilgün <ilgun.ilgun@gmail.com>
2015-03-31 23:54:06 +0300

    Made status interface better, it should be queried by elevatorId instead of looping all statuses.

--- src/test/java/elevator/ElevatorControlSystemImplTest.java
+++ src/test/java/elevator/ElevatorControlSystemImplTest.java
@@ -32,20 +32,25 @@ public class ElevatorControlSystemImplTest {
     }
 
     @Test
+    public void returnAbsentIfElevatorIsNotPresent() {
+        assertThat(system.getStatus(10).isPresent(), is(false));
+    }
+
+    @Test
     public void systemCanHandlePickUpRequests() {
         system.pickup(3, 0, -1);
         system.pickup(3, 4, 1);
         system.pickup(3, 2, 1);
 
         system.step();
-        ElevatorStatus status = system.getStatus().get(3);
+        ElevatorStatus status = system.getStatus(3).get();
 
         assertThat(status.getCurrentFloor(), is(2));
         assertThat(status.getDestinationFloor(), is(4));
 
         system.step();
 
-        ElevatorStatus latestStatus = system.getStatus().get(3);
+        ElevatorStatus latestStatus = system.getStatus(3).get();
 
         assertThat(latestStatus.getCurrentFloor(), is(4));
         assertThat(latestStatus.getDestinationFloor(), is(0));
@@ -57,7 +62,7 @@ public class ElevatorControlSystemImplTest {
         system.pickup(2, 2, 1);
         system.step();
 
-        ElevatorStatus status = system.getStatus().get(2);
+        ElevatorStatus status = system.getStatus(2).get();
 
         assertThat(status.getElevatorId(), is(2));
         assertThat(status.getCurrentFloor(), is(2));
@@ -68,13 +73,13 @@ public class ElevatorControlSystemImplTest {
     public void systemCanJumpToNextStep() {
         system.step();
 
-        ElevatorStatus firstElevatorStatus = system.getStatus().get(0);
+        ElevatorStatus firstElevatorStatus = system.getStatus(0).get();
 
         assertThat(firstElevatorStatus.getElevatorId(), is(0));
         assertThat(firstElevatorStatus.getCurrentFloor(), is(1));
         assertThat(firstElevatorStatus.getDestinationFloor(), is(1));
 
-        ElevatorStatus secondElevatorStatus = system.getStatus().get(1);
+        ElevatorStatus secondElevatorStatus = system.getStatus(1).get();
 
         assertThat(secondElevatorStatus.getCurrentFloor(), is(2));
         assertThat(secondElevatorStatus.getDestinationFloor(), is(2));
@@ -82,14 +87,14 @@ public class ElevatorControlSystemImplTest {
 
     @Test
     public void elevatorSystemCanUpdateElevator() {
-        ElevatorStatus initialStatus = system.getStatus().get(0);
+        ElevatorStatus initialStatus = system.getStatus(0).get();
 
         assertThat(initialStatus.getElevatorId(), is(0));
         assertThat(initialStatus.getCurrentFloor(), is(0));
         assertThat(initialStatus.getDestinationFloor(), is(1));
 
         system.update(0, 1, 2);
-        ElevatorStatus updatedStatus = system.getStatus().get(0);
+        ElevatorStatus updatedStatus = system.getStatus(0).get();
 
         assertThat(updatedStatus.getCurrentFloor(), is(1));
         assertThat(updatedStatus.getDestinationFloor(), is(2));
